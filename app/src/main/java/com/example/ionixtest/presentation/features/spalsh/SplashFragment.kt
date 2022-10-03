e0a37b8a4805ca967708ed9950537461c84d90ea
Ibrahim Estanga <ibrahim.estanga@mercadolibre.com.co>
2022-10-02 21:00:20 -0500

    permission request

--- app/src/main/java/com/example/ionixtest/presentation/features/spalsh/SplashFragment.kt
+++ app/src/main/java/com/example/ionixtest/presentation/features/spalsh/SplashFragment.kt
@@ -1,10 +1,15 @@
 package com.example.ionixtest.presentation.features.spalsh
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.ionixtest.R
@@ -13,6 +18,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class SplashFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,9 +29,38 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch {
-            delay(2000)
-            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+            } else {
+                checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+            }
         }
+    }
+
+    // Function to check and request permission.
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(requireActivity(), permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            requestPermissions(arrayOf(permission), requestCode)
+        } else {
+            lifecycleScope.launch {
+                delay(2000)
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToHomeFragment())
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
     }
 }
